483ea74e5616b5bce85a999060ee46ddf51b7737
axtrace <sofinma@ya.ru>
2019-08-09 22:36:18 +0300

    add telebot

--- telebot.py
+++ telebot.py
@@ -1,14 +1,30 @@
-import telebot
+from telegram.ext import Updater
 from token_loader import TokenLoader
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 
 t = TokenLoader()
 token = t.get('telegram_token')
-bot = telebot.TeleBot(token)
+updater = Updater(token=token)
+
+dispatcher = updater.dispatcher
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+def start(update, context):
+    print('get ', update.message)
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="I'm a bot, please talk to me!")
 
 
-bot.polling()
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=update.message.text)
+
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
